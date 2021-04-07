11b8e3dd4ca22f022afec35eb1c451d172f311d3
junCodeSnapshot <154179code@gmail.com>
2021-04-06 18:37:25 -0700

    docs agregado

---
+++ docs/precache-manifest.e13bc6a0d529ef486a23aecf2031f2e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d2697f1bf83972966f1ccabf8e4ae3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "695f12865a25844b31af",
+    "url": "/static/css/main.fed278df.chunk.css"
+  },
+  {
+    "revision": "21128ddbf1ff63946978",
+    "url": "/static/js/2.31a10122.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.31a10122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "695f12865a25844b31af",
+    "url": "/static/js/main.f9067232.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
